cdfc885fa9d4f82ca186dcb802a43a15b04f2a28
AndreGTHB <andregthb@gmail.com>
2020-11-02 18:57:32 +0100

    Updates

---
+++ precache-manifest.f57d668e4a56ee34a021527d99f93b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6777808ac3730a7666399363945225e4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e5831c3da35da360e8d5",
+    "url": "/monsters/static/css/main.fad30802.chunk.css"
+  },
+  {
+    "revision": "0ebb722af33ec599140e",
+    "url": "/monsters/static/js/2.c27b6d93.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/monsters/static/js/2.c27b6d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5831c3da35da360e8d5",
+    "url": "/monsters/static/js/main.d9dc114c.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
